38dff0d1641da9f5eeb852260da4ec200c2ab7ab
hangocmy <hangocmy00@gmail.com>
2023-02-24 17:29:56 +0700

    git

--- AppiumTesting/testcases/1-test_webapplication.py
+++ AppiumTesting/testcases/1-test_webapplication.py
@@ -2,15 +2,18 @@ from datetime import time
 
 from appium import webdriver
 
-desired_caps = dict(
-    deviceName = 'sdk_gphone64_x86_64',
-    uid = 'emulator-5554',
-    platformName = 'Android',
-    browserName = 'Chrome'
-)
+desired_caps = {
+  "appium:deviceName": "Galaxy J7 Pro",
+  "platformName": "Android",
+  "appium:platformVersion": "9",
+  "appium:automationName": "uiautomator2",
+  "appium:appPackage": "com.android.chrome",
+}
 
 driver = webdriver.Remote('http://127.0.0.1:4723', desired_caps)
 
 driver.get("https://google.com")
 print(driver.title)
+
 time.sleep(2)
+driver.quit()
