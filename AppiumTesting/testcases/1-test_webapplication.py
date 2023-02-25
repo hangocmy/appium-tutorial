deeb7e4e7715d8d0dd887338b9f6046cbf6b1077
hangocmy <hangocmy00@gmail.com>
2023-02-25 14:52:14 +0700

    idb

--- AppiumTesting/testcases/1-test_webapplication.py
+++ AppiumTesting/testcases/1-test_webapplication.py
@@ -1,19 +1,25 @@
 from datetime import time
 
 from appium import webdriver
+from appium.webdriver.common.mobileby import MobileBy
 
 desired_caps = {
-  "appium:deviceName": "Galaxy J7 Pro",
+  "appium:deviceName": "OPPO",
+  "appium:uid": "OV6PEQ5PSGGEOBWW",
   "platformName": "Android",
-  "appium:platformVersion": "9",
-  "appium:automationName": "uiautomator2",
-  "appium:appPackage": "com.android.chrome",
+  "appium:platformVersion": "10",
+  "appium:ignoreHiddenApiPolicyError": "true",
+  "appium:noRetest": "true",
+  "appium:automationName": "UiAutomator2",
+  "appPackage": "com.idbeauty",
+  "appActivity": "com.idbeauty.MainActivity"
 }
 
-driver = webdriver.Remote('http://127.0.0.1:4723', desired_caps)
-
-driver.get("https://google.com")
-print(driver.title)
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+customer_code = driver.find_element(MobileBy.CLASS_NAME, "android.widget.EditText")
+customer_code.clear()
+customer_code.send_keys("Demo")
+#driver.find_element(MobileBy.XPATH, "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[3]").click();
 
 time.sleep(2)
 driver.quit()
